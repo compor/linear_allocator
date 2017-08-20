86256fd040a486cd1962dac62df49bdfb895eba7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-20 23:17:08 +0100

    add separate propagation traits

---
+++ include/propagation_traits.hpp
@@ -0,0 +1,18 @@
+#ifndef ICSA_PROPAGATION_TRAITS
+#define ICSA_PROPAGATION_TRAITS
+
+#include <type_traits>
+// using std::false_type
+
+namespace icsa {
+
+template <typename T>
+struct propagation_traits {
+  using propagate_on_container_copy_assignment = std::false_type;
+  using propagate_on_container_move_assignment = std::false_type;
+  using propagate_on_container_swap = std::false_type;
+};
+
+}  // namespace icsa end
+
+#endif  // header
