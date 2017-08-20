28d5c94a23ca10517f31bfcc0c78083e4e61e04e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-20 23:17:08 +0100

    add unit test

---
+++ unittests/test_private_memory_arena.cpp
@@ -0,0 +1,34 @@
+//
+//
+//
+
+#include "linear_allocator/private_memory_arena.hpp"
+
+#include <cassert>
+// using assert
+
+#include <csignal>
+
+#include "gtest/gtest.h"
+// using testing::Test
+
+namespace {
+
+class test_private_memory_arena : public testing::Test {
+ public:
+  test_private_memory_arena() {}
+
+ protected:
+};
+
+TEST_F(test_private_memory_arena, zero_allocation_segfault) {
+  const std::size_t n = 0;
+  icsa::private_memory_arena<n> pma;
+
+  auto *raw = pma.allocate(sizeof(int), alignof(int));
+  auto *ptr = reinterpret_cast<int *>(raw);
+
+  EXPECT_EXIT({ int k = *ptr; }, ::testing::KilledBySignal(SIGSEGV), ".*");
+}
+
+}  // namespace anonymous end
