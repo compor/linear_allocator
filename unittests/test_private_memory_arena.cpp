8572844f463912b2f79fe72a5175f082491b4deb
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-20 23:17:08 +0100

    add unit test case

--- unittests/test_private_memory_arena.cpp
+++ unittests/test_private_memory_arena.cpp
@@ -31,4 +31,14 @@ TEST_F(test_private_memory_arena, zero_allocation_segfault) {
   EXPECT_EXIT({ int k = *ptr; }, ::testing::KilledBySignal(SIGSEGV), ".*");
 }
 
+TEST_F(test_private_memory_arena, allocation_segfault) {
+  const std::size_t n = 1;
+  icsa::private_memory_arena<n> pma;
+
+  auto *raw = pma.allocate(sizeof(int), alignof(int));
+  auto *ptr = reinterpret_cast<int *>(raw);
+
+  EXPECT_EXIT({ int k = *ptr; }, ::testing::KilledBySignal(SIGSEGV), ".*");
+}
+
 }  // namespace anonymous end
