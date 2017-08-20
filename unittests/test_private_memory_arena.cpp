59ac776fef69e9104c262d6dcc879474f41e2395
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-20 23:17:08 +0100

    update unit test

--- unittests/test_private_memory_arena.cpp
+++ unittests/test_private_memory_arena.cpp
@@ -7,11 +7,16 @@
 #include <cstdlib>
 // using std::exit
 
+#include <new>
+// using std::bad_alloc
+
 #include <csignal>
 // using SIGSEGV
 
 #include "gtest/gtest.h"
 // using testing::Test
+
+#include <iostream>
 
 namespace {
 
@@ -26,6 +31,10 @@ class test_private_memory_arena : public testing::Test {
   }
 };
 
+TEST_F(test_private_memory_arena, zero_allocation_segfault) {
+  ASSERT_THROW({ icsa::private_memory_arena<0> pma; }, std::bad_alloc);
+}
+
 TEST_F(test_private_memory_arena, exact_size_allocation) {
   using alloc_t = int;
   icsa::private_memory_arena<sizeof(alloc_t)> pma;
@@ -36,9 +45,10 @@ TEST_F(test_private_memory_arena, exact_size_allocation) {
   EXPECT_EXIT(access(p2), ::testing::ExitedWithCode(0), ".*");
 }
 
-TEST_F(test_private_memory_arena, zero_allocation_segfault) {
-  icsa::private_memory_arena<0> pma;
+TEST_F(test_private_memory_arena, allocation_segfault) {
+  icsa::private_memory_arena<1> pma;
   using alloc_t = int;
+  static_assert(sizeof(alloc_t) > 1, "sizeof int must be larger than 1");
 
   auto *p1 = pma.allocate(sizeof(alloc_t), alignof(alloc_t));
   auto *p2 = reinterpret_cast<alloc_t *>(p1);
@@ -46,15 +56,21 @@ TEST_F(test_private_memory_arena, zero_allocation_segfault) {
   EXPECT_EXIT(access(p2), ::testing::KilledBySignal(SIGSEGV), ".*");
 }
 
-TEST_F(test_private_memory_arena, allocation_segfault) {
-  icsa::private_memory_arena<1> pma;
+TEST_F(test_private_memory_arena, multiple_allocation_segfault) {
   using alloc_t = int;
-  static_assert(sizeof(alloc_t) > 1, "sizeof int must be larger than 1");
+  const std::size_t elements_number = 10;
+  const std::size_t n = elements_number * sizeof(alloc_t);
+  std::cerr << n << std::endl;
+  icsa::private_memory_arena<n> pma;
 
-  auto *p1 = pma.allocate(sizeof(alloc_t), alignof(int));
+  auto *p1 = pma.allocate(n, alignof(alloc_t));
+  std::cerr << p1 << std::endl;
   auto *p2 = reinterpret_cast<alloc_t *>(p1);
 
-  EXPECT_EXIT(access(p2), ::testing::KilledBySignal(SIGSEGV), ".*");
+  p2 += elements_number + 10000000;
+  std::cerr << p2 << std::endl;
+
+  EXPECT_EXIT(access(p2), ::testing::ExitedWithCode(0), ".*");
 }
 
 }  // namespace anonymous end
