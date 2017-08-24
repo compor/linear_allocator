3a218c5dc26268dafefa26c5d4f4785081850751
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-24 18:09:33 +0100

    add unit test case

--- unittests/test_linear_allocator.cpp
+++ unittests/test_linear_allocator.cpp
@@ -11,6 +11,9 @@
 #include <vector>
 // using std::vector
 
+#include <list>
+// using std::list
+
 #include <algorithm>
 // using std::equal
 
@@ -32,11 +35,27 @@ TEST_F(test_linear_allocator, vector_allocation) {
 
   std::array<int, 9> src{3, 99, 1001, 5, 32, 973, 973, 32, 5};
 
-  using lpa_t = icsa::memory::linear_private_allocator<alloc_t, 150>;
+  using lpa_t = imem::linear_private_allocator<alloc_t, 150>;
 
   lpa_t::storage_type s;
   lpa_t lpa{s};
   std::vector<alloc_t, lpa_t> dst{lpa};
+
+  for (const auto &e : src) dst.push_back(e);
+
+  EXPECT_TRUE(std::equal(src.begin(), src.end(), dst.begin()));
+}
+
+TEST_F(test_linear_allocator, list_allocation) {
+  using alloc_t = int;
+
+  std::array<int, 9> src{3, 99, 1001, 5, 32, 973, 973, 32, 5};
+
+  using lpa_t = imem::linear_private_allocator<alloc_t, 450>;
+
+  lpa_t::storage_type s;
+  lpa_t lpa{s};
+  std::list<alloc_t, lpa_t> dst{lpa};
 
   for (const auto &e : src) dst.push_back(e);
 
