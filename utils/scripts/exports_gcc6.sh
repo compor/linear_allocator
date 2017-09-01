8cda8cb1df42f09ab9eb7e6c95644db5d3309438
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-01 11:55:28 +0100

    add export scripts for various gcc versions

---
+++ utils/scripts/exports_gcc6.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export CC=gcc-6
+export CXX=g++-6
+
+export GTEST_ROOT=/usr/local/gtest-libstdcxx/
+
+export CXX_FLAGS=""
+
+export LINKER_FLAGS="" 
+
+export BUILD_TYPE=Debug
+
+export PRJ_SKIP_TESTS=OFF
+export PRJ_DEBUG=ON
+
